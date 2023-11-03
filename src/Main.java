18aa7b691f56ec4388437f7adb84287803374d0b
Sofia-Sonia-Martinez <sofiasoniamar99@gmail.com>
2023-11-03 17:34:34 -0300

    feature: add exercice class 2 to main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import EjercicioClase2.Alumno;
+import EjercicioClase2.Inscripcion;
+import EjercicioClase2.Materia;
 import Relaciones.Auto;
 import Relaciones.Domicilio;
 import Relaciones.Persona;
 import Relaciones.Curso;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,5 +18,32 @@ public class Main {
 
         System.out.println("¿Tiene domicilio?:" + domicilio.getTieneDomicilio());
 
+        //Ejercicio clase 2
+        Materia materia1 = new Materia();
+        Materia materia2 = new Materia();
+        List<Materia> materiasCorrelativas = new ArrayList<>();
+        materiasCorrelativas.add(materia1);
+        materiasCorrelativas.add(materia2);
+
+        Materia materia = new Materia();
+        materia.setCorrelativas(materiasCorrelativas);
+        Alumno alumno = new Alumno();
+        List<Materia> materiasAlumno = new ArrayList<>();
+        materiasAlumno.add(materia);
+        materiasAlumno.add(materia1);
+        //Si saco la materia 2 por ejemplo me da false, si la agrego me da true.
+        //materiasAlumno.add(materia2);
+        alumno.setMaterias(materiasAlumno);
+        Inscripcion inscripcion = new Inscripcion(materia,alumno);
+        boolean bool = inscripcion.inscripcion();
+        if(bool){
+            System.out.println("true, se inscribio");
+        }else{
+            System.out.println("false, no se inscribio");
+        }
+
+
     }
+
+
 }
